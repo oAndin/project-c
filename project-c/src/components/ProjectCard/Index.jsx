b5bed0406cbe898fe3f78f29520e99d7806a3087
oAndin <00andherson@gmail.com>
2023-10-19 22:26:34 -0300

    update tailwind

--- project-c/src/components/ProjectCard/Index.jsx
+++ project-c/src/components/ProjectCard/Index.jsx
@@ -1,7 +1,40 @@
-import React from 'react'
-import { bsPencil, bsFillTrashFill } from 'react-icons/bs';
+import React, { useEffect, useRef } from 'react'
+import { BsPencil, BsFillTrashFilll, BsFillTrashFill } from 'react-icons/bs';
+import { Link } from 'react-router-dom';
 
 const ProjectCard = ({ id, name, budget, category, handleRemove }) => {
+
+    let colorCategory = useRef('');
+
+    switch (colorCategory) {
+        case colorCategory.current === 'infrastructure':
+            colorCategory = 'bg-infrastructure'
+            break;
+        case colorCategory.current === 'development':
+            colorCategory = 'bg-development'
+            break;
+        case colorCategory.current === 'design':
+            colorCategory = 'bg-design'
+            break;
+        default: colorCategory = 'bg-planning';
+    }
+
+    // if(colorCategory === 'Infrastructure') {
+    //     colorCategory = 'bg-infrastructure'
+    // }
+    // if(colorCategory === 'Development') {
+    //     colorCategory = 'bg-development'
+    // }
+    // if(colorCategory === 'Design') {
+    //     colorCategory = 'bg-design'
+    // }
+    // if(colorCategory === 'Planning') {
+    //     colorCategory = 'bg-planning'
+    // }
+
+    useEffect(()=> {
+        console.log((category))
+    },[])
     return (
         <>
             <div className='border-2 flex flex-col p-8'>
@@ -9,7 +42,11 @@ const ProjectCard = ({ id, name, budget, category, handleRemove }) => {
                 <p>{name}</p>
                 <p>{budget}</p>
                 <div className='flex justify-center items-center gap-3'>
-                    <span className='w-2 h-2 bg-black rounded-full'></span><p>{category}</p>
+                    <span className={`w-2 h-2 bg-black rounded-full ${colorCategory}`}></span><p>{category}</p>
+                </div>
+                <div id="cardActions">
+                    <Link to='/'><BsPencil/>Editar</Link>
+                    <Link to='/'><BsFillTrashFill/>Remover</Link>
                 </div>
             </div>
         </>
