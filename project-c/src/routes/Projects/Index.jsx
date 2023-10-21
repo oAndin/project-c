6079cfc248540803c2b29cfe4869fed4465a52ff
oAndin <00andherson@gmail.com>
2023-10-20 22:39:09 -0300

    BUG fix loader comp

--- project-c/src/routes/Projects/Index.jsx
+++ project-c/src/routes/Projects/Index.jsx
@@ -59,7 +59,7 @@ const Projects = () => {
               )
             }
             {!removeLoading && <Loader />}
-            {!removeLoading && projects.length === 0
+            {!removeLoading && projects.length === 0 &&
                (<p> You dont have any projects yet!</p>) 
             }
         </div>
