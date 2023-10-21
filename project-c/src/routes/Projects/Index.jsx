aa534197566fa1ab73ff21d1b3047eb43fdeded4
oAndin <00andherson@gmail.com>
2023-10-20 23:41:47 -0300

    FIX loader-comp when length 0

--- project-c/src/routes/Projects/Index.jsx
+++ project-c/src/routes/Projects/Index.jsx
@@ -59,7 +59,7 @@ const Projects = () => {
               )
             }
             {!removeLoading && <Loader />}
-            {projects.length === 0 && !removeLoading && 
+            {projects.length === 0 && removeLoading && 
                (<p> You dont have any projects yet!</p>) 
             }
         </div>
