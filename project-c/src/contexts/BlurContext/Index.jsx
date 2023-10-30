1dada1c579affef4d64f26060f950ceef0bdd95c
oAndin <00andherson@gmail.com>
2023-10-30 18:11:32 -0300

    update script creating-context

--- project-c/src/contexts/BlurContext/Index.jsx
+++ project-c/src/contexts/BlurContext/Index.jsx
@@ -1,3 +1,13 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const BlurContext = createContext();
+
+export const BlurProvider = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <BlurContext.Provider value={{ isOpen, setIsOpen }}>
+      {children}
+    </BlurContext.Provider>
+  )
+}
