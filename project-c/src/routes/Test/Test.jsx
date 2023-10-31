e75049e74857b3cb8bc32bf4dad4d5f8d8611416
oAndin <00andherson@gmail.com>
2023-10-31 01:07:10 -0300

    BTN LOGIC

--- project-c/src/routes/Test/Test.jsx
+++ project-c/src/routes/Test/Test.jsx
@@ -1,19 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import BlurModal from '../../components/BlurModal/Index';
 import ButtonOpenModal from '../../components/ButtonOpenModal/Index';
+import { BlurContext } from '../../contexts/BlurContext/Index';
 
 const Test = () => {
+
+  const {isOpen} = useContext(BlurContext);
 
   return (
     <>
       <div className='h-screen'>
-        <ButtonOpenModal></ButtonOpenModal>
         <h1>This is just a simple test</h1>
         <h3>Nothing to see here</h3>
         <h6>Move along!</h6>
         <p>Thank you! :)</p>
         <BlurModal/>  
-        <ButtonOpenModal/>
+        {!isOpen && (<ButtonOpenModal/>)}
       </div>
     </>
   );
